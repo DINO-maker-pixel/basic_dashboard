4d158813224ebf4111b55730d49c34b514f234bb
Dino <rlacksdnd951@gmail.com>
2024-10-31 13:16:37 +0900

    미니프로젝트_꽃

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+# 데이터 생성
 data = {
-    '이름' : ['무궁화', '튤립', '해바라기', '아이리스', '수선화'],
-    '나라' : ['대한민국', '네덜란드', '러시아', '프랑스', '파키스탄'],
-    '개화시기' : [7, 4, 8, 5, 12] 
-    }
+    '이름': ['무궁화', '튤립', '해바라기', '아이리스', '수선화'],
+    '나라': ['대한민국', '네덜란드', '러시아', '프랑스', '파키스탄'],
+    '개화시기': [7, 4, 8, 5, 12]
+}
 
 df = pd.DataFrame(data)
 
@@ -13,8 +14,11 @@ df = pd.DataFrame(data)
 st.title('Flowers Data Dashboard')
 st.write('간단히 몇 가지의 꽃들을 소개합니다 :')
 
-# 대시보드에 데이터프레임 표시
-st.write(df)
+# 나라 선택 기능 추가
+selected_country = st.selectbox("나라를 선택하세요", df['나라'].unique())
+filtered_df = df[df['나라'] == selected_country]
+st.write(f"{selected_country}의 꽃 정보:")
+st.write(filtered_df)
 
 # 개화시기에 관한 간단한 시각화
 st.write("꽃들의 개화시기에 관한 그래프")
